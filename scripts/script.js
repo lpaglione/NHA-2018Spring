ae8af4aa2e541fed14648dfe49506e859d582a5e
JordanPag <JordanPag@users.noreply.github.com>
2018-02-19 13:50:07 -0500

    Challenge 8

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,13 @@
 
 //Test to see if this is working - put a message into the console
 console.log("Javascript file is correctly linked!")
+
+//function to hide the image
+$(".hide").click( () => {
+  $(".img").hide();
+});
+
+//function to show the image
+$(".show").click( () => {
+  $(".img").show();
+});
